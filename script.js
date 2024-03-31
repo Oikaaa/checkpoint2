c95509aee386fcf42a0bc55bd40898e4b7b76839
Oikaaa <jimmythang2017@gmail.com>
2024-04-01 00:05:52 +1100

    hello

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const name = document.getElementById('name').value
-const job = document.getElementById('job').value
-const userId = document.getElementById('userId').value
 const GET = document.getElementById('GET')
 const POST = document.getElementById('POST')
 const DELETE = document.getElementById('DELETE')
 const PUT = document.getElementById('PUT')
 const text = document.getElementById('text')
-
-const newUser = {
-    name: 'hello',
-    job: 'por'
-}
 
 GET.addEventListener('click', ()=>{
     fetch('https://reqres.in/api/user')
@@ -27,30 +19,59 @@ GET.addEventListener('click', ()=>{
 })
 
 POST.addEventListener('click', ()=>{
+    const userName = document.getElementById('name').value
+    const job = document.getElementById('job').value
+    const newUser = {
+        name: userName,
+        job: job
+    }
     console.log(newUser)
-    fetch('https://reqres.in/api/users',{
-        redirect: 'follow',
-        mode: 'no-cors',
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newUser)
-    })
-    .then (response => console.log(response))
-    .catch(e => console.log(e))
+    
+        fetch('https://reqres.in/api/user', {
+            mode: 'no-cors',
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+        . then (response => 
+            console.log(response.body))
+        .catch(e => console.log(e))
+
 })
 
 PUT.addEventListener('click', ()=>{
+    const userId = document.getElementById('userId').value
+    const userName = document.getElementById('name').value
+    const job = document.getElementById('job').value
+    const newUser = {
+        name: userName,
+        job: job
+    }
+    console.log(newUser)
+    if(userId === ""){
+        alert('Id incorrect')
+    }else{
+        fetch('https://reqres.in/api/user/' + userId,{
+            mode: 'no-cors',
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+        . then (response => 
+            console.log(response.body))
+        .catch(e => console.log(e))
+    }
+})
+
+DELETE.addEventListener('click', ()=>{
+    const userId = document.getElementById('userId').value
     fetch('https://reqres.in/api/user/' + userId,{
-        mode: 'no-cors',
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newUser)
+        method: 'DELETE',
     })
-    . then (response => response.json())
-    . then (data => text.innerText = data)
+    . then (response => console.log(response))
     .catch(e => console.log(e))
 })
